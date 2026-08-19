da14f486ac76c893737e981951bc642b29d531b9
byun1416-INS <byun1416@gmail.com>
2026-08-19 22:01:14 +0900

    Update secure-config.js

--- secure-config.js
+++ secure-config.js
@@ -3,4 +3,4 @@
 window.INS_AUTH_WEBHOOK_URL = "https://hook.eu1.make.com/eo2ikalnd6hxp21umo4f2en85duo886w";
 
 // 접수완료용 Make webhook. 인증 웹훅과 같은 시나리오로 합쳐도 되고 별도로 써도 됩니다.
-window.INS_UPLOAD_WEBHOOK_URL = "";
+window.INS_UPLOAD_WEBHOOK_URL = "https://hook.eu1.make.com/939iye59yv0m85kcowbjp4hzh2dqnu3t";
